53f1fba6016a632b817152bb51e0d334701fc912
kushwahavikas045 <vikas9106931320@gmail.com>
2022-02-03 11:57:52 +0530

    images support added in webpack

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,12 @@
 import Data from "./Data";
+import taxi from '../images/yellow-taxi-cab.png';
 const App = () => {
 
 
     return (
         <div className='app'>app components, data
          <Data/>
+         <img src={taxi} alt='image' width="250"/>
         </div>
     );
 };
--- webpack.config.js
+++ webpack.config.js
@@ -18,6 +18,10 @@ module.exports = {
         }
       },
     mode: mode,
+    output: {
+        // this places all images processed in an image folder
+        assetModuleFilename: "images/[hash][ext][query]",
+      },
     module: {
         rules: [
             {
@@ -29,8 +33,17 @@ module.exports = {
             },
             {
                 test: /\.(s[ac]|c)ss$/i,
-                use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
+                use: [{
+                    loader: MiniCssExtractPlugin.loader,
+                    options: {publicPath: ""},
+                },
+                'css-loader',
+                'sass-loader'],
             },
+            {
+                test: /\.(png|jpe?g|gif|svg)$/i,
+                type: "asset",
+            }
         ],
     },
     plugins: [new MiniCssExtractPlugin()],
